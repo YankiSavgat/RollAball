0d6957ed909ad4b3e4510f5d62243d9b3aa65a6a
Yankı <Savgat>
2020-10-16 10:21:46 +0300

    Initialize

---
+++ RollABall/Assets/Scripts/CoinController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+// Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 2, -90) * Time.deltaTime);
+    }
+}
---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float ForceMultiplier;
+    
+    private float _moveHorizontal, _moveVertical;
+    private Rigidbody _rigidbody;
+    
+    // Each time is script is loaded
+    private void Awake()
+    {
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // Update is called 60 times per second
+    // Good for physics
+    private void FixedUpdate()
+    {
+        _moveHorizontal = Input.GetAxis("Horizontal") * ForceMultiplier ;
+        _moveVertical= Input.GetAxis("Vertical");
+        
+        _rigidbody.AddForce(_moveHorizontal, 0, _moveVertical * ForceMultiplier);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ScoreObject"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
